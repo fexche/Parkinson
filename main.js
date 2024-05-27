45cef5e899801f6ca582bc627f07d1f8c8673513
fexche <ing_fexche@hotmail.com>
2024-05-26 18:33:40 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
 
-class CjaSalidaCaja{
-      numeroDocumento;
-      Fecha;
-      MontoMonedaNac
-}
 
-let _salidacaja= new CjaSalidaCaja();
-
-_salidacaja.numeroDocumento="1500023"
-_salidacaja.Fecha=Date()
-_salidacaja.MontoMonedaNac=150.00
-
-// Agregar propiedades 
-_salidacaja.Concepto="CJA006-DEPOSITO DESDE CAJA GENERAL"
-// Eliminar Propiedades 
-delete _salidacaja.Concepto
-
-console.log(_salidacaja)
+alert("La resolución de tu pantalla es: " + screen.width + " x " + screen.height) 
